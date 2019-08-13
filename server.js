6a5e12ec02f69982cdc36078c68193f3b33dfcbc
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-13 12:26:48 -0500

    Create GET handler for /api/users

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
-
+const express = require('express');
+const userRouter = require('./users/userRouter.js')
+const postsRouter = require('./posts/postRouter.js')
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+// ROUTER
+server.use('/api/users', userRouter )
 
+// CUSTOM MIDDLEWARE // 
 function logger(req, res, next) {
-
+  console.log(`${req.method}`)
+  next()
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,22 +1,51 @@
-const express = 'express';
+const express = require('express')
+const Users = require('./userDb')
+const Posts =  require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
 
+router.post('/', (req, res) => {
+   
 });
 
 router.post('/:id/posts', (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
 
+//GET
+router.get('/', async (req, res) => {
+   try {
+       const users = await Users.get()
+       res.status(200).json({ users })
+   } catch (error) {
+     console.log(error)
+     res.status(500).json({message: 'Error retrieving the users'})
+   }
 });
 
-router.get('/:id', (req, res) => {
 
+
+router.get('/:id', async (req, res) => {
+  try {
+    const hub = await Users.getById(req.params.id);
+
+    if (hub) {
+      res.status(200).json(hub);
+    } else {
+      res.status(404).json({ message: 'Hub not found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the hub',
+    });
+  }
 });
+
+
 
 router.get('/:id/posts', (req, res) => {
 
@@ -45,3 +74,5 @@ function validatePost(req, res, next) {
 };
 
 module.exports = router;
+
+
